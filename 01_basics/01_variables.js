444583eda66dd966b95b50ea3fb8a14e0762a770
swarnim-925 <135423817+swarnim-925@users.noreply.github.com>
2024-08-29 20:33:26 +0000

    discussion on variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 1899
+let accountEmail = "swarnimsharma30@gmail.com"
+var accountPassword = "12345"
+accountCity = "Narsinghpur"
+let accountState;
+
+console.log(accountCity);
+console.table([accountId,accountCity,accountEmail,accountPassword,accountState])
